48cc9ee55c09cd1c6049b1aa50fefd84008aaf77
LaityH <1831495788@qq.com>
2021-08-08 12:13:08 +0800

    修复BDownloader选择文件保存路径后不重启显示无效的问题

--- src/renderer/multipages/page1/pages/BDownloaderSetting.vue
+++ src/renderer/multipages/page1/pages/BDownloaderSetting.vue
@@ -86,6 +86,7 @@ export default {
               if (err2) {this.$message.error('保存失败！', 2)}
               this.$message.success('保存成功！', 1)
               // console.log(2)
+              window.BDownloaderSetting = fileData
             })
           })
         }
